12594d930184ba1920de1befd9f49c2d452c9596
Tienlong Pham <aglorei88@gmail.com>
2015-04-29 14:31:21 -0700

    stable file structure

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,5 +5,3 @@ class Comment < ActiveRecord::Base
 
 	validates :content, presence: true, length: { minimum: 2 }
 end
-
-# User.find(1).posts.find(1).comments.create(content: "Hey, you know what? This is pretty cool. - Tienlong", user: User.find(2), commenter: User.find(1))
